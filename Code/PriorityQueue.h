6df5fa1900c045ab55df7c39baa3d77f23a44a13
srinidhi <srinidhiprabhu96@gmail.com>
2017-11-07 23:16:38 +0530

    Parallel Priority Queue insert done:

--- Code/PriorityQueue.h
+++ Code/PriorityQueue.h
@@ -1,13 +1,18 @@
 #define R 3
 #define QSIZE 1000
+#define NUM_BITS 10
+#include <iostream>
+#include <stdio.h>
+using namespace std;
 #ifndef _KERNELS_H_
 #define _KERNELS_H_
 
 class Node{
+public:
 	int key;
 	int val;
 
-public:	
+	
 	__host__ __device__
 	Node(int a,int b){
 		key = a;
@@ -31,11 +36,12 @@ public:
 };
 
 class PQNode{
+public:
 	Node nodes[R];
 	int level;
 	int size;
 	
-public:	
+	
 	__host__ __device__
 	PQNode(Node *a, int len, int c){
 		int min = (len < R)?len:R;
@@ -54,19 +60,136 @@ public:
 };
 
 class PriorityQueue{
+public:	
 	PQNode nodes[QSIZE];
+	int sizes[QSIZE];
 	int curr_size;
-	
-public:
+
+
 	__host__ __device__
 	PriorityQueue(){
 		curr_size = 0;
+		for(int i=0;i<QSIZE;i++){
+			sizes[i] = 0;
+		}
 	}
 	
 	__host__ __device__
 	int getSize(){
 		return curr_size;
 	}
+	
+	__host__ __device__
+	int getInsertTarget(){
+		int ans = -1;
+		for(int i=0;i<QSIZE;i++){
+			if(sizes[i]!=R){
+				ans = i;
+				break;
+			}
+		}
+		return ans;
+	}
+	
+	__host__ __device__
+	void writeToNode(int *arr, int size, int index){
+		int orig_size = nodes[index].size;
+		for(int i=0;i<size&&nodes[index].size<R;i++){
+			nodes[index].nodes[nodes[index].size].key = arr[i];
+			nodes[index].size++;
+		}
+		if(orig_size == 0){
+			curr_size++;
+		}
+	}
+	
+	__host__
+	void print_object() {
+		for(int i=0;i<curr_size;i++){
+			cout << "Node number " << i << endl;
+			for(int j=0;j<nodes[i].size;j++){
+				cout << nodes[i].nodes[j].key << " ";
+			}
+			cout << endl;
+		}
+		cout << "Printed object" << endl;
+	}
+	
+};
+
+class InsertTable{
+public:
+	int status[QSIZE]; // Says whether the entry in the table is in use or not.
+	int indices[QSIZE]; // Index of the node in the priority queue on which the process should happen.
+	int elements[QSIZE][R]; // Set of elements to be inserted at the node.
+	int num_elements[QSIZE];
+	int level[QSIZE]; // Level number of the node on which the operation occurs.
+	int target[QSIZE]; // Target node to insert the set of elements.
+	int target_bits[QSIZE][NUM_BITS];
+	
+
+	__host__ __device__
+	InsertTable(){
+		for(int i=0;i<QSIZE;i++){
+			status[i] = 0;
+			num_elements[i] = 0; 
+		}
+	}
+	
+	__host__ __device__
+	void addEntry(int index,int *elmts,int size,int tgt){
+		int off = 0;
+		while(status[off]!=0){
+			off++;
+		}
+		status[off] = 1;
+		indices[off] = index;
+		int min = (size < R)?size:R;
+		for(int i=0;i<min;i++){
+			elements[off][i] = elmts[i];
+			num_elements[off]++;
+		}
+		target[off] = tgt;
+		int val = 0,o1 = index;
+		while(o1 > 0){
+			val++;
+			o1/=2;
+		}
+		level[off] = val;
+		int num = 0,rem;
+		tgt+=1;
+		while(tgt > 0){
+			rem = tgt%2;
+			tgt/=2;
+			num = num*2+rem;
+		}
+		int count = 0;
+		while(num>0){
+			num/=2;
+			target_bits[off][count++] = num%2;
+		}
+	}
+	
+	__host__ __device__
+	void printTable(){
+		int count = 0;
+		for(int i=0;i<QSIZE;i++){
+			if(status[i] == 1){
+				printf("Index: %d\n",indices[i]);
+				printf("Elements: ");
+				for(int j=0;j<num_elements[i];j++){
+					printf("%d ",elements[i][j]);
+				}
+				printf("\nLevel: %d\n",level[i]);
+				printf("Target: %d\n",target[i]);
+				printf("##########\n");
+				count++;
+			}
+		}
+		printf("Done XXXX %d\n\n",count);
+	}
+	
+
 };
 
 #endif
--- Code/pq_kernels.h
+++ Code/pq_kernels.h
@@ -1,4 +1,74 @@
 #include <stdio.h>
-__global__ void print(PriorityQueue *pq){
+
+__global__ void print_val(PriorityQueue *pq)
+{
 	printf("%d %d\n",threadIdx.x,pq->getSize());
+}
+
+__global__ void insert(PriorityQueue *pq, InsertTable *table,int* offsets, int size){
+	int index = threadIdx.x +blockIdx.x*blockDim.x;
+	if(index < size){
+		int offset = offsets[index];
+		if(table->indices[offset] == table->target[offset]){
+			
+			
+			pq->writeToNode(table->elements[offset],table->num_elements[offset],table->target[offset]);
+			
+			table->status[offset] = 0;
+			table->num_elements[offset] = 0;
+		}
+		else{
+			int arr[2*R];
+			int tot_size = 0;
+			int tmp;
+			for(int i=0;i<table->num_elements[offset];i++){
+				arr[tot_size++] = table->elements[offset][i];
+			}
+			int node_num = table->indices[offset];
+			for(int i=0;i<pq->nodes[node_num].size;i++){
+				arr[tot_size++] = pq->nodes[node_num].nodes[i].key;
+			}
+			for(int i=0;i<tot_size;i++){
+				for(int j=i+1;j<tot_size;j++){
+					if(arr[i] > arr[j]){
+						tmp = arr[i];
+						arr[i] = arr[j];
+						arr[j] = tmp;
+					}
+				}
+			}
+			for(int i=0;i<R&&i<tot_size;i++){
+				pq->nodes[node_num].nodes[i].key = arr[i];
+			}
+			if(tot_size>R){
+				for(int i=R;i<tot_size;i++){
+					table->elements[offset][i-R] = arr[i];
+				}
+			}
+			// Change the entry so that it moves towards the target.
+			int level = table->level[offset];
+			int target = table->target[offset];
+			int rem;
+			rem = table->target_bits[offset][level];
+			table->indices[offset] = (node_num)*2+1+rem;
+			table->level[offset] += 1;
+			/*if(table->target[offset]==8){
+				printf("Hello ");
+				for(int i=0;i<10;i++){
+					printf("%d ",table->target_bits[offset][i]);
+				}
+				printf("\n");
+			}
+			if(table->target[offset]==4)printf(" Hello %d %d %d %d\n",node_num,(node_num)*2+1+rem,table->target[offset],level);*/
+			table->num_elements[offset] = (tot_size>R)?tot_size-R:0;
+		}
+	}
+}
+
+__global__ void writeToNode(PriorityQueue* pq, InsertTable *table, int *offsets, int size){
+	int index = threadIdx.x +blockIdx.x*blockDim.x;
+	if(index < size){
+		int offset = offsets[index];
+		pq->writeToNode(table->elements[offset],table->num_elements[offset],table->target[offset]);
+	}
 }
